0f05b47543e886a7b08bbd35d6a1c1bb8f561024
flololan <flo.s.weiss@gmail.com>
2019-02-05 11:15:28 +0100

    Kontaktdaten geändert

--- src/components/Congratulation.js
+++ src/components/Congratulation.js
@@ -6,16 +6,16 @@ export function Congratulation(props){
             <p>Félicitation vous avez résolu le problème. <br/><br/>
                 Pour toute questions sur cette étude, merci de contacter Guillaume Gronier, chercheur en UX Design (guillaume.gronier@gmail.com)
                 <br/><br/>
-                Pour toute questions concernant l'application, rendez-vous sur <a href="https://github.com/dederobert/tours_de_hanoi" rel="noopener noreferrer" target="_blank">GitHub <i className="fab fa-github" title="GitHub du projet"></i></a>
+                Pour toute questions concernant l'application, rendez-vous sur <a href="https://github.com/flololan/tours_de_hanoi" rel="noopener noreferrer" target="_blank">GitHub <i className="fab fa-github" title="GitHub du projet"></i></a>
             </p>
             <br/>
             <h3>Partager</h3>
             <div id="share-buttons">
-                <a href="http://www.facebook.com/sharer.php?u=https://dederobert.github.io/tours_de_hanoi/"
+                <a href="http://www.facebook.com/sharer.php?u=https://flololan.github.io/tours_de_hanoi/"
                    target="_blank" rel="noopener noreferrer" >
                     <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook"/>
                 </a>
-                <a href="https://twitter.com/share?url=https://dederobert.github.io/tours_de_hanoi/&amp;hashtags=TourDeHanoi"
+                <a href="https://twitter.com/share?url=https://flololan.github.io/tours_de_hanoi/&amp;hashtags=TourDeHanoi"
                    target="_blank" rel="noopener noreferrer" >
                     <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter"/>
                 </a>
